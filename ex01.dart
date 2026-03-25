148e28f6d8d99775bbdd80272b8d3975fbb15eb6
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-03-25 18:30:11 -0300

    Create ex01.dart

---
+++ ex01.dart
@@ -0,0 +1,29 @@
+class Pessoa {
+  
+  String name;  
+  int idade;
+  
+  Pessoa(this.name ,this.idade){
+      if(this.name == ""){
+        throw ("Nome não pode ser vazio.");
+      } 
+      if(this.idade <= 0){
+        throw("Idade tem que ser maior que zero.");
+      }
+    return ;
+  }
+  
+  void exibirDados(){
+    print(" Nome: $name \n Idade: $idade \n\n");
+  }
+}
+
+void main() {
+    Pessoa p1 = Pessoa("Jhonatan",25);
+    Pessoa p2 = Pessoa("Jhenifer",24);
+    Pessoa p3 = Pessoa("Bernadete",49);
+  
+    p1.exibirDados();
+    p2.exibirDados();
+    p3.exibirDados();
+}
