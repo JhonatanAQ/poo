902fc246228408c719bb3a7bbf58c3f9fd6c17a0
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-03-25 22:05:01 -0300

    Update ex04.dart

--- ex04.dart
+++ ex04.dart
@@ -7,8 +7,8 @@ class Lampada {
   
 
   void ligar (){
-   
-    if(quantidadeLigacoes >= 5 ){
+     // quantidadeLigacoes inicia em 0 logo tem que subitrair por 4 .
+    if(quantidadeLigacoes >= 4 ){
       ligada = false ;
       queimada = true;
       print("A lâmpada está queimada e não pode ser ligada.");
@@ -32,16 +32,15 @@ class Lampada {
   void exibirEstado(){
 
     print("\n\nA lâmpada \n\t- Está ${ligada ? "ligada" : "desligada"}.");
-    print("\t- Foi ligada $quantidadeLigacoes X.");
-    print("\t- E ${ligada ? "não esta queimada":"está queimada"}.");
+    // quantidadeLigacoes inicia em 0 logo tem que subitrair por + 1.
+    print("\t- Foi ligada ${quantidadeLigacoes + 1} X."); 
+    print("\t- E ${queimada ?"está queimada":"não esta queimada"}.");
   }
 }
 
 void main() {
   Lampada l1 = Lampada();
   l1.ligar();
-  l1.ligar();
-  l1.desligar();
   l1.ligar();
   l1.desligar();
   l1.ligar();
