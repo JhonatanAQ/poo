1b1fd658792bb6dbe0f3bf32209b6d58a00bb1d4
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-03-25 21:56:49 -0300

    Update ex04.dart

--- ex04.dart
+++ ex04.dart
@@ -1,31 +1,56 @@
-ex04.dartclass Lampada {
+class Lampada {
   bool ligada ;
   int quantidadeLigacoes;
   bool queimada ;
 
-  Lampada(){
-    ligada = false;
-    quantidadeLigacoes = 0;
-    queimada = false ;
-  }
+  Lampada():ligada = false,quantidadeLigacoes = 0, queimada = false ;
+  
 
-  void logar (){
-    this.ligada = true;
-  }
-  void desligar(){
-    this.ligada = false;
-  }
-  void exibirEstado(){
-    if(ligada){
-      print("Ligada");
-    }else{
-      print("Desligada");
+  void ligar (){
+   
+    if(quantidadeLigacoes >= 5 ){
+      ligada = false ;
+      queimada = true;
+      print("A lâmpada está queimada e não pode ser ligada.");
+      return ;
     }
+    if(!ligada){
+      ligada = true;
+      print("Lâmpada ligada.");
+      quantidadeLigacoes = quantidadeLigacoes + 1; 
+    }else{
+      print("Lâmpada já está ligada.");
+    }
+    
+  }
+  
+  void desligar(){
+      ligada = false;
+      print("Lâmpada desligada.");
+  }
+  
+  void exibirEstado(){
+
+    print("\n\nA lâmpada \n\t- Está ${ligada ? "ligada" : "desligada"}.");
+    print("\t- Foi ligada $quantidadeLigacoes X.");
+    print("\t- E ${ligada ? "não esta queimada":"está queimada"}.");
   }
 }
 
 void main() {
-  Lampada d1 = Lampada(01, 04, 2010);
-  d1.exibirHorario();
+  Lampada l1 = Lampada();
+  l1.ligar();
+  l1.ligar();
+  l1.desligar();
+  l1.ligar();
+  l1.desligar();
+  l1.ligar();
+  l1.desligar();
+  l1.ligar();
+  l1.desligar();
+  l1.ligar();
+  l1.desligar();
+  l1.ligar();
+  l1.exibirEstado();
 
 }
