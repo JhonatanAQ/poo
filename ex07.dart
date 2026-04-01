0dfdf9027e0043464966449c43ef2afb17660459
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-04-01 16:09:46 -0300

    Update ex07.dart

--- ex07.dart
+++ ex07.dart
@@ -1,1 +1,43 @@
 
+class Contador {
+  int _valor = 0;
+
+  void incrementar() {
+    _valor++;
+    print('Contador: $_valor');
+  }
+
+  void decrementar() {
+    if (_valor > 0) {
+      _valor--;
+      print('Contador: $_valor');
+    } else {
+      print('Não é possível decrementar, o valor já é 0.');
+    }
+  }
+
+  void zerar() {
+    _valor = 0;
+    print('Contador: $_valor');
+  }
+  void exibirValor() {
+    print('Contador: $_valor');
+  }
+}
+
+void main() {
+  Contador contador1 = Contador();
+  
+  contador1.exibirValor(); 
+  contador1.incrementar(); 
+  contador1.incrementar();  
+  contador1.exibirValor();  
+  contador1.decrementar();  
+  contador1.exibirValor();  
+  contador1.zerar();        
+  contador1.exibirValor();  
+  contador1.decrementar();  
+  contador1.exibirValor(); 
+
+ 
+}
