5d5ec9080123acb44671e77ffa3f993c7ee8dbbc
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-04-01 19:36:12 -0300

    Update ex14.dart

--- ex14.dart
+++ ex14.dart
@@ -1,15 +1,23 @@
+
 class Motor {
   int potencia;
   String tipo;
 
-  Motor(this.potencia,this.tipo);
+  Motor(this.potencia, this.tipo) {
+    if (potencia <= 0) { throw('A potência do motor deve ser maior que 0. Valor recebido: $potencia');}
+    if (tipo.isEmpty){throw('O tipo de motor não pode ser vazio.');}
+  }
 }
 
 class Carro {
   String _modelo;
   Motor _motor;
 
-  Carro(this._modelo,this._motor);
+  Carro(this._modelo, this._motor) {
+    if (_modelo.isEmpty) {
+      throw ('O modelo do carro não pode ser vazio.');
+    }
+  }
 
   void exibirMotor() {
     print('  Potência do Motor: ${_motor.potencia} HP');
@@ -19,19 +27,19 @@ class Carro {
   void exibirCarro() {
     print('Modelo do Carro: $_modelo');
     print('Detalhes do Motor:');
-    exibirMotor(); 
+    exibirMotor();
   }
 }
 
 void main() {
 
-  Motor motorEsportivo = Motor(250,'Gasolina Premium');
-  Motor motorEletrico = Motor(180,'Elétrico');
-  
-  Carro carroEsportivo = Carro('Sport GT',motorEsportivo);
-  Carro carroUrbano = Carro('Eco EV', motorEletrico);
+    Motor motorEsportivo = Motor(250, 'Gasolina Premium');
+    Motor motorEletrico = Motor(180, 'Elétrico');
 
-  carroEsportivo.exibirCarro();
-  print("\n");
-  carroUrbano.exibirCarro();
+    Carro carroEsportivo = Carro('Sport GT', motorEsportivo);
+    Carro carroUrbano = Carro('Eco EV', motorEletrico);
+
+    carroEsportivo.exibirCarro();
+    print('');
+    carroUrbano.exibirCarro();
 }
