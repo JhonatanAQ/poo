eab8b494080739dbbc1e700937b6ea553b226027
JhonatanAQ <jhonatanjaq.study@gmail.com>
2026-04-13 20:20:21 -0300

    Update ex02.dart

--- ex02.dart
+++ ex02.dart
@@ -1,30 +1,34 @@
-class Relogio {
-  int _hora;
-  int _minuto;
-  int _segundo;
+class Produto {
+  String _nome;
+  double _preco; 
+  int _estoque;
   
-  Relogio(this._hora ,this._minuto,this._segundo){
-      if(this._hora < 0 && this._hora > 23 ){
-        throw ("Horas deve estar entre 0 e 23.");
-      } 
-      if(this._minuto < 0 && this._minuto > 59){
-        throw("Minuto deve estar entre 0 e 59.");
-      }
-      if(this._segundo < 0 && this._segundo > 59){
-        throw("Segundo deve estar entre 0 e 59.");
-      }
+  Produto(this._nome, this._preco, this._estoque);
+  
+  void repor(int qtd){
+    _estoque = _estoque + qtd;
+    print("Reposição realizada:${qtd} ${_nome}");
+    print("e no estoque tem apenas: ${_estoque} unidade \n");
   }
-  void exibirHorario(){
-    print("\n$_hora: $_minuto: $_segundo ");
+  void vender(int qtd){
+    if(qtd >_estoque){
+      print("No estoque tem apenas : ${_estoque}");
+      return;
+    }else{
+      print("Compra realizada:${qtd} ${_nome}.");
+      _estoque = _estoque - qtd;
+      exibirFicha();
+    }
+  }
+  void exibirFicha(){
+    print("${_nome} por ${_preco} R\$ a unidade. ");
+    print("e no estoque tem apenas: ${_estoque} unidade \n");
   }
 }
 
 void main() {
-   Relogio r1 = Relogio(15,35,28);    
-   Relogio r2 = Relogio(16,36,29);
-   Relogio r3 = Relogio(15,35,28);
-  
-   r1.exibirHorario();
-   r2.exibirHorario();
-   r3.exibirHorario();
+  Produto p1 = Produto("Caneta",2.6,100);
+  p1.vender(8);
+  p1.vender(12);
+  p1.repor(30);
 }
