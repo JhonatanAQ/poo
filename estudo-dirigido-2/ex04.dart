726159c5fe6c948435f264a5af27f90aef2f7eb1
LABCODEJHOW <jhonatanjaq.study@gmail.com>
2026-05-04 19:31:00 -0300

    ex04 feito

--- estudo-dirigido-2/ex04.dart
+++ estudo-dirigido-2/ex04.dart
@@ -3,12 +3,15 @@ class ContaBancaria {
   double _saldo = 0;
   ContaBancaria(this._titular);
   
-  double get(){
-    return this._saldo;
-  }
-  void set(double valor){
-    this._saldo = valor;
-  }
+  double get saldo => _saldo;
+  
+  set saldo(double valor) {
+    if (valor >= 0) {
+      _saldo = valor;
+    } else {
+      print("Erro: O saldo não pode ser negativo.");
+    }
+   }
   void depositar(double valor){
     this._saldo = _saldo + valor;
   }
@@ -20,32 +23,44 @@ class ContaBancaria {
     }
   }
   void exibirSaldo(){
-    print("Saldo da cont é de $_saldo");
+    print("$_titular seu saldo da conta  é de R\$ $_saldo ");
   }
   
 }
 class ContaPoupanca extends ContaBancaria{
   double taxaRendimento = 1;
   ContaPoupanca(String titular) : super(titular);
-  
-  @override
+
   void aplicarRendimento(){
-    double saldo = super.get();
-    double rendimento = saldo / 100 * taxaRendimento;
-    super.set(rendimento);
+    double rendimento = super.saldo * (taxaRendimento / 100);
+    super.saldo = super.saldo + rendimento;
   }
 }
 class ContaCorrente extends ContaBancaria{
   double taxaRendimento = 5;
   ContaCorrente(String titular) : super(titular);
   
-  @override
   void aplicarRendimento(){
-    double saldo = super.get();
-    double rendimento = saldo / 100 * taxaRendimento;
-    super.set(rendimento);
+    double rendimento = super.saldo * (taxaRendimento / 100);
+    super.saldo = super.saldo + rendimento;
   }
 }
 void main() {
-    print('hello ');
+  ContaBancaria contaBancaria = ContaBancaria("Jhonatan-1");
+  ContaPoupanca contaPoupanca = ContaPoupanca("Jhonatan-2");
+  ContaCorrente contaCorrente = ContaCorrente("Jhonatan-3");
+  void exibir(){
+    contaBancaria.exibirSaldo();
+    contaPoupanca.exibirSaldo();
+    contaCorrente.exibirSaldo();
+    print("-"*45);
+  }
+  exibir();
+  contaBancaria.depositar(5.5);
+  contaPoupanca.depositar(7.5);
+  contaCorrente.depositar(2.3);
+  exibir();
+  contaPoupanca.aplicarRendimento();
+  contaCorrente.aplicarRendimento();
+  exibir();
 }
